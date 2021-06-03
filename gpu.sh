4febca5bf3fd86e80b16cb761486e50f6939e879
babehdyo <widyo.babeh@gmail.com>
2021-06-03 16:13:53 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=us-east.ezil.me:5555
-WALLET=0x378b6d537ceec6864ef522df428b9fca08f0b888.zil185l0fk2h4vsvc884jmcjzz7df0rtpqtzdhp3ll
+POOL=daggerhashimoto.eu-west.nicehash.com:3353
+WALLET=35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Colab)
 
 chmod +x tuyulgpu
